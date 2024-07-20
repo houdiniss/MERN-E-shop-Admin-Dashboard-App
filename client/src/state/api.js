482c234d8e7518ba3afec67c5bc72dac1fe035c1
Giorgos <giorgosmintis@gmail.com>
2024-07-20 09:30:39 +0300

    edit

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,7 +1,7 @@
 import { createApi , fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5001" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://mern-e-shop-admin-dashboard-app.onrender.com" }),
   reducerPath: "adminApi",
   tagTypes: [
     "User",
